44d0678267ae7b11be9fb8313a3c52c8a8b93a31
Marcelo Selong <marceloselong1@gmail.com>
2022-04-29 12:14:07 -0300

    Realização de exercícios do dia 29/04/2022.

---
+++ exer048.py
@@ -0,0 +1,9 @@
+#Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de 3,
+#e que se encontram no intervalo de 1 até 50.
+s = 0
+print('Os número ímpares e múltiplos de \033[33m3\033[m são: ')
+for c in range(0, 50+1):
+    if c % 2 == 1 and c % 3 == 0:
+        s += c
+        print(c, '', end="")
+print('A soma entre estes número é: \033[31m{}\033[m.'.format(s))
