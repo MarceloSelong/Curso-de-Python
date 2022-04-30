44ce32fbbf740fe2ba3902b33f5c767520ac8ea7
Marcelo Selong <marceloselong1@gmail.com>
2022-04-30 16:52:25 -0300

    Realização de exercícios do dia 30/04/2022.

---
+++ exer052.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+n = int(input('Informe um número inteiro: '))
+if n == 1:
+    print('O numéro {} não é primo.'.format(n))
+elif n == 2 or n == 3 or n == 5 or n == 7:
+    print('O número {} é primo.'.format(n))
+elif n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n % 7 == 0:
+    print('O número {} não é primo.'.format(n))
+else:
+    print('O número {} é primo.'.format(n))
