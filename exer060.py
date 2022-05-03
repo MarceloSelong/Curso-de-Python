776698088d74a05699fb2516733b754b617701d2
Marcelo Selong <marceloselong1@gmail.com>
2022-05-03 11:59:18 -0300

    Realização de exercícios do dia 03/05/2022.

--- exer060.py
+++ exer060.py
@@ -1,27 +1,16 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # Ex: 5!=5x4x3x2x1=120
-# Fazer um com While e outro com for
-
+print('---FATORIAL---')
 n = int(input('Informe um valor: '))
-resfat = n * (n - 1)
-for c in range(n - 2, 0, -1):
-    resfat = resfat * c
-print('5!=', end='')
-for c in range(n, 0, -1):
+c = n-1 #Contador
+resultado = n #Resultado final do fatorial
+print('{}!={}'.format(n, n), end='x') #Principal
+while c > 0:
+    resultado *= c
     if c != 1:
-        print(c, end="x")
+        print(c, end='x')
     else:
-        print('{}={}'.format(c, resfat))
-print('FIM')
+        print('{}={}'.format(c, resultado))
+    c -= 1
 print()
-n = str(input('Informe um valor: '))
-print(n+'!='+n, end='x')
-cont = int(n)
-result = int(n)
-while cont != 1:
-    result = result * (cont-1)
-    cont -= 1
-    if cont != 1:
-        print(cont, end="x")
-    else:
-        print('{}={}'.format(cont, result))
+print(resultado)
