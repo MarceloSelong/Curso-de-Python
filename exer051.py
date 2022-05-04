a970bf753a96b6cb476a1407a84e1c1f0a2e4273
Marcelo Selong <marceloselong1@gmail.com>
2022-05-04 00:02:27 -0300

    Realização de exercícios do dia 04/05/2022.

--- exer051.py
+++ exer051.py
@@ -2,7 +2,26 @@
 #No final, mostre os 10 primeiros termos dessa progressão.
 a1 = int(input('Informe o primeiro termo da P.A.: '))
 r = int(input('Informe a razão da P.A.: '))
-x = 10 * r +1
-print('Os 10 primeiro termos dessa P.A. são: ')
-for c in range(a1, x, r):
-    print(c, end=', ')
+x = a1 + (10-1) * r
+
+if r == 0:
+    print('Esta é uma P.A. \033[31mconstante\033[m.')
+    for c in range(0, 10):
+        if c == 9:
+            print('{}.'.format(r))
+        else:
+            print(r, end=', ')
+elif r > 0:
+    print('Os 10 primeiro termos dessa P.A. \033[31mcrescente\033[m são: ')
+    for c in range(a1, x+1, r):
+        if c == x:
+            print(c, end='.')
+        else:
+            print(c, end=', ')
+else:
+    print('Os 10 primeiros termos dessa P.A. \033[31mdecrescente\033[m são: ')
+    for c in range(a1, x-1, r):
+        if c == x:
+            print(c, end='.')
+        else:
+            print(c, end=', ')
