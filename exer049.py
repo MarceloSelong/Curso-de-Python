250ee50ec9a27a65c17ecbc7b23431eb32f9db6d
Marcelo Selong <marceloselong1@gmail.com>
2022-05-09 13:59:28 -0300

    Tabuada 2.0

--- exer049.py
+++ exer049.py
@@ -1,6 +1,6 @@
 #Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher,
 #utilizando um laço 'for'.
 n = int(input('Informe o número que deseja saber a tabuada: '))
-print('Tabuada do', n)
+print('Tabuada do', n, end=':\n')
 for c in range(0, 10+1):
     print('{} x {:2} = {}'.format(n, c, n*c))
