95410210553cf3b5afc0bcabd50a7179374bf362
Marcelo Selong <marceloselong1@gmail.com>
2022-05-08 23:22:24 -0300

    Liquidação

--- exer012.py
+++ exer012.py
@@ -1,3 +1,3 @@
 print('Liquidação 5% de desconto')
 preco = float(input('Informe o preço do produto: '))
-print('O novo preço deste produto será de: \033[4;31mR${:.2f}\033[m'.format(preco-preco/100*5))
+print('O novo preço deste produto com desconto de 5% será de: \033[4;31mR${:.2f}\033[m'.format(preco-preco/100*5))
