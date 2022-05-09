154e3d048c8dc7d97721b06c0a91f8649c2b40b4
Marcelo Selong <marceloselong1@gmail.com>
2022-05-08 22:49:16 -0300

    Soma entre variáveis.

--- exer003.py
+++ exer003.py
@@ -5,4 +5,5 @@ cores = {'limpa': '\033[m',
 n1 = int(input('Informe dois valores:'))
 n2 = int(input(''))
 s = n1 + n2
-print('A soma entre {}{}{} e {}{}{} é igual a {}{}{}.'.format(cores['amarelo'], n1, cores['limpa'], cores['azul'], n2, cores['limpa'], cores['roxo'], s, cores['limpa']))
+print('A soma entre {}{}{} e {}{}{} é igual a {}{}{}.'.format(cores['amarelo'], n1, cores['limpa'], cores['azul'],
+                                                              n2, cores['limpa'], cores['roxo'], s, cores['limpa']))
