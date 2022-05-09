2871a435101b341b7e5eb7550f4172442fb418d3
Marcelo Selong <marceloselong1@gmail.com>
2022-05-09 13:41:33 -0300

    Verificador de multa 1.0

--- exer029.py
+++ exer029.py
@@ -5,6 +5,7 @@
 vel = int(input('Informe a velocidade que passou no radar. '))
 multa = (vel - 80)*7
 if vel > 80:
-    print('Você foi multado. A multa será no valor de: R${}.'.format(multa))
+    print('Você foi multado. Ultrapassou 80km/h, será adicionado R$7,00 por cada KM acime do limite. '
+          'A multa será no valor de: R${}.'.format(multa))
 else:
     print('Você não foi multado.')
