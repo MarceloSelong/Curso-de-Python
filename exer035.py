8f95b910e9fb9eb8b3f845b799e159693a6c7fb6
Marcelo Selong <marceloselong1@gmail.com>
2022-05-09 13:47:47 -0300

    Cálculo de triângulos

--- exer035.py
+++ exer035.py
@@ -1,6 +1,6 @@
 #Desenvolva um programa que leia o comprimento de três retas.
 #E diga ao usuário se ela podem ou não formar um triângulo.
-r1 = int(input('Informe 3 valores: '))
+r1 = int(input('Informe o comprimento de 3 retas para calcularmo triângulo: '))
 r2 = int(input())
 r3 = int(input())
 if r1+r2 > r3 and r1+r3 > r2 and r3+r2 > r1:
