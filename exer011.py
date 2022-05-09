8e293b73971bfc3ed3b9b2408af89e545e2215b5
Marcelo Selong <marceloselong1@gmail.com>
2022-05-08 23:21:39 -0300

    Cálculo de tinta necessária.

--- exer011.py
+++ exer011.py
@@ -3,3 +3,4 @@ l = float(input('Informe a \033[34mlargura\033[m da parede em \033[31mmetros\033
 a = float(input('Informe a \033[34maltura\033[m: '))
 print('A quantidade de tinta necessária para pintar esta parede de \033[34m{}m²\033[m são \033[31m{}\033[m '
       'litros de tinta. Sabendo que \033[34m2L\033[m pintam \033[31m2m²\033[m.'.format(l*a, (l*a)/2))
+print('Fim')
