f8694c09ccc1d12d78e548e08ae3d41e884e361e
Marcelo Selong <marceloselong1@gmail.com>
2022-05-10 15:24:56 -0300

    Soma, multiplicação, maior e menor com menu de escolha

--- exer059.py
+++ exer059.py
@@ -7,6 +7,7 @@
 #[5]sair do programa
 
 #Seu programa deverá realizar a operação solicitada em cada caso.
+from time import sleep
 v1 = input('Informe o primeiro valor: ')
 v2 = input('Informe o segundo valor: ')
 v1 = float(v1.replace(',', '.'))
@@ -22,26 +23,28 @@ while escolha != 5:
     [5]sair do programa """))
     print()
     if escolha == 1:
-        print('A soma entre \033[34m{}\033[m e \033[34m{}\033[m é igual a: \033[31m{}\033[m.'.format(v1, v2, v1+v2))
+        print('A soma entre \033[34m{:.0f}\033[m e \033[34m{:.0f}\033[m é igual a: \033[31m{:.0f}\033[m.'.format(v1, v2, v1+v2))
         print()
     elif escolha == 2:
-        print('A multiplicação entre \033[34m{}\033[m e \033[34m{}\033[m é igual a: \033[31m{}\033[m.'.format(v1 , v2, v1*v2))
+        print('A multiplicação entre \033[34m{:.0f}\033[m e \033[34m{:.0f}\033[m é igual a: \033[31m{:.0f}\033[m.'.format(v1 , v2, v1*v2))
         print()
     elif escolha == 3:
         if v1 > v2:
-            print('O maior número digitado entre \033[34m{}\033[m e \033[34m{}\033[m é: \033[31m{}\033[m.'.format(v1, v2, v1))
+            print('O maior número digitado entre \033[34m{:.0f}\033[m e \033[34m{:.0f}\033[m é: \033[31m{:.0f}\033[m.'.format(v1, v2, v1))
             print()
         elif v1 < v2:
-            print('O maior número digitado entre \033[34m{}\033[m e \033[34m{}\033[m é: \033[31m{}\033[m.'.format(v1, v2, v2))
+            print('O maior número digitado entre \033[34m{:.0f}\033[m e \033[34m{:.0f}\033[m é: \033[31m{:.0f}\033[m.'.format(v1, v2, v2))
             print()
         else:
-            print('Os números \033[34m{}\033[m e \033[34m{}\033[m são \033[31miguais\033[m.'.format(v1, v2))
+            print('Os números \033[34m{:.0f}\033[m e \033[34m{:.0f}\033[m são \033[31miguais\033[m.'.format(v1, v2))
             print()
     elif escolha == 4:
         v1 = input('Informe o primeiro valor: ')
         v2 = input('Informe o segundo valor: ')
         v1 = float(v1.replace(',', '.'))
         v2 = float(v2.replace(',', '.'))
+        print()
     elif escolha == 5:
         print('\033[31mENCERRANDO...\033[m')
         escolha = 5
+        sleep(2)
