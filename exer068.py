2cb6230a8f05bf83671865079bd77668d8b884c2
Marcelo Selong <marceloselong1@gmail.com>
2022-05-15 19:17:18 -0300

    Jogo de par ou ímpar

--- exer068.py
+++ exer068.py
@@ -1,9 +1,36 @@
+# Faça um programa que jogue par ou ímpar com o computador. O jogo só erá interrompido quando o jogador perder.
+# Mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+from random import randint
+print('=-'*20)
+print('VAMOS JOGAR PAR OU ÍMPAR')
+print('=-' * 20)
+ganhas = 0
 while True:
-    num = int(input('Gostaria de ver a tabuada de qual número? '))
-    print('-'*30)
-    if num < 0:
-        break
-    for c in range(1, 11):
-        print(f'{num} x {c} = {num*c}')
-    print('-'*30)
-print('\033[31mEncerrando...\033[m')
+    valor = int(input('Digite um valor: '))
+    pc = randint(0, 11)
+    escolha = input('Escolhe par ou ímpar? [P/I]')
+    print('=-' * 20)
+    while escolha not in 'PpIi':
+        print('Escolha corretamente: ')
+        escolha = input('Escolhe par ou ímpar? [P/I]')
+
+    if (pc+valor) % 2 == 0:
+        print(f'Você jogou {valor} e o computador {pc}. Total de {pc+valor}, deu par.')
+        if escolha in 'Pp':
+            print('\033[33mVocê ganhou!\033[m')
+            ganhas += 1
+            print('=-' * 20)
+        else:
+            print(f'\033[31mGame over!\033[m Você venceu {ganhas} vezes.')
+            print('=-' * 20)
+            break
+    else:
+        print(f'Você jogou {valor} e o computador {pc}. Total de {pc + valor}, deu ímpar.')
+        if escolha in 'Ii':
+            print('\033[33mVocê ganhou!\033[m')
+            ganhas += 1
+            print('=-' * 20)
+        else:
+            print(f'\033[31mGame over!\033[m Você venceu {ganhas} vezes.')
+            print('=-' * 20)
+            break
