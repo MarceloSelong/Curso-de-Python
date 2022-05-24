b2fd253783d0aa517bf870e5146be3af7a5cb0ab
Marcelo Selong <marceloselong1@gmail.com>
2022-05-24 13:50:03 -0300

    Lista de preços com Tupla

---
+++ exer076.py
@@ -0,0 +1,8 @@
+# Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+# No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+listagem = ('Arroz', 22, 'Feijão', 4.5, 'Massa', 3, 'Açúcar', 18)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>5.2f}')
